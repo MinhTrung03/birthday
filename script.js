c2df7d9b1c4a60332ad0cb4200db0988542b0b30
MinhTrung03 <dangminhtrung2007@gmail.com>
2026-09-25 01:09:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -69,16 +69,16 @@ window.addEventListener("load", function () {
 });
 
 const photos = [
-    "image/1.jpeg",
-    "image/2.jpeg",
-    "image/3.jpeg",
-    "image/4.jpeg",
-    "image/5.jpeg",
-    "image/6.jpeg",
-    "image/7.jpeg",
-    "image/8.jpeg",
-    "image/9.jpeg",
-    "image/10.jpeg",
+    "image/1.jpg",
+    "image/2.jpg",
+    "image/3.jpg",
+    "image/4.jpg",
+    "image/5.jpg",
+    "image/6.jpg",
+    "image/7.jpg",
+    "image/8.jpg",
+    "image/9.jpg",
+    "image/10.jpg",
 ];
 let current = 0;
 
